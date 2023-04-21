22a272a2d4e1bfc0fa8c3f18185363bb53f4eb6c
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-04-21 12:33:32 +0300

    Update main.php

--- main.php
+++ main.php
@@ -3,5 +3,10 @@
 require_once 'vendor/autoload.php';
 
 $client = new App\ApiClient();
+$cryptoCurrencies = $client->getInfo();
 
-$CryptoCurrencyInfo = $client->getInfo();
+foreach ($cryptoCurrencies as $cryptoCurrency) {
+    echo "Name: " . $cryptoCurrency->getName() . PHP_EOL;
+    echo "Symbol: " . $cryptoCurrency->getSymbol() . PHP_EOL;
+    echo "Price: $" . $cryptoCurrency->getPrice() . PHP_EOL;
+}
