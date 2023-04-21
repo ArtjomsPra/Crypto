dce5c87cdc3eaf814b19424c79ec3a9f24deb339
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-04-21 12:34:06 +0300

    Update ApiClient.php

--- App/ApiClient.php
+++ App/ApiClient.php
@@ -1,25 +1,31 @@
 <?php declare(strict_types=1);
 
 namespace App;
+
 use GuzzleHttp\Client;
 
 class ApiClient {
-
     private $client;
 
-public function __construct () {
-    $this->client =new Client(['verify' => false]);
-}
+    public function __construct()
+    {
+        $this->client = new Client(['verify' => false]);
+    }
 
-public function getInfo () : array {
-    $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&convert=USD';
-    $response = $this->client->request('GET', $url, [
-        'headers' => [
-            'Accepts' => 'application/json',
-            'X-CMC_PRO_API_KEY' => '333febdf-4fef-4216-8a84-cfe9ed412462'
-        ]
-    ]);
-    $cryptoCurrencyInfo = json_decode($response->getBody()->getContents());
-    return $cryptoCurrencyInfo->data;
-}
+    public function getInfo(): array
+    {
+        $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&convert=USD';
+        $response = $this->client->request('GET', $url, [
+            'headers' => [
+                'Accepts' => 'application/json',
+                'X-CMC_PRO_API_KEY' => '333febdf-4fef-4216-8a84-cfe9ed412462'
+            ]
+        ]);
+        $cryptoCurrencyData = json_decode($response->getBody()->getContents(), true)['data'];
+        $cryptoCurrencies = [];
+        foreach ($cryptoCurrencyData as $currencyData) {
+            $cryptoCurrencies[] = new CryptoCurrencyInfo($currencyData);
+        }
+        return $cryptoCurrencies;
+    }
 }
