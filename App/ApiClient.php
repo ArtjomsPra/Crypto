4e58b4946b271335f1ebddd55479add67dea20e5
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-04-21 12:21:33 +0300

    Add files via upload

---
+++ App/ApiClient.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App;
+use GuzzleHttp\Client;
+
+class ApiClient {
+
+    private $client;
+
+public function __construct () {
+    $this->client =new Client(['verify' => false]);
+}
+
+public function getInfo () : array {
+    $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&convert=USD';
+    $response = $this->client->request('GET', $url, [
+        'headers' => [
+            'Accepts' => 'application/json',
+            'X-CMC_PRO_API_KEY' => '333febdf-4fef-4216-8a84-cfe9ed412462'
+        ]
+    ]);
+    $cryptoCurrencyInfo = json_decode($response->getBody()->getContents());
+    return $cryptoCurrencyInfo->data;
+}
+}
---
+++ App/CryptoCurrencyInfo.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+class CryptoCurrencyInfo {
+private string $name;
+private string $symbol;
+private float $price;
+
+public function __construct(string $name, string $symbol, float $price)
+{
+    $this->name = $name;
+    $this->symbol = $symbol;
+    $this->price = $price;
+}
+
+public function getName () : string {
+    return $this->name;
+}
+
+    public function getSymbol () : string {
+        return $this->symbol;
+    }
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
